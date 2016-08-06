3e3ef9eedd5da2e0cc041faafa293dc749e0b4af
Iris Wang <iris@pinterest.com>
2016-08-06 16:55:09 -0700

    fixed styling issues

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -1,11 +1,20 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.mysimpletweets.models.Tweet;
+import com.codepath.apps.mysimpletweets.utils.ConvertJsonDate;
+import com.squareup.picasso.Picasso;
+
+
+import org.w3c.dom.Text;
 
 
 import java.util.ArrayList;
@@ -16,16 +25,28 @@ import java.util.ArrayList;
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
 
     public TweetsArrayAdapter(Context context, ArrayList<Tweet> tweets) {
-        super(context, android.R.layout.simple_list_item_1, tweets);
+        super(context, 0, tweets);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Tweet tweet = getItem(position);
         if (convertView == null) {
-
-
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
         }
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        TextView tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+        TextView tvTimeStamp = (TextView) convertView.findViewById(R.id.tvTimeStamp);
+
+        tvTimeStamp.setText(ConvertJsonDate.getRelativeTimeAgo(tweet.getCreatedAt()));
+        tvUsername.setText("@" + tweet.getUser().getScreenName());
+        tvName.setText(tweet.getUser().getName());
+        tvBody.setText(tweet.getBody());
+        ivProfileImage.setImageResource(android.R.color.transparent);
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
+
         return convertView;
     }
 }
