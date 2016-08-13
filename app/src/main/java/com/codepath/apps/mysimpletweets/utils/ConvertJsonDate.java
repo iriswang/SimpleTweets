3731880eed0c1ceb963441467bec6de2f1277a51
Iris Wang <iris@pinterest.com>
2016-08-13 14:26:33 -0700

    customized time difference converter

--- app/src/main/java/com/codepath/apps/mysimpletweets/utils/ConvertJsonDate.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/utils/ConvertJsonDate.java
@@ -11,6 +11,11 @@ import java.util.Locale;
  * Created by iris on 8/6/16.
  */
 public class ConvertJsonDate {
+    private  static final int MINUTE_IN_SECONDS = 60;
+    private  static final int HOUR_IN_SECONDS = 60 * MINUTE_IN_SECONDS;
+    private static final int DAY_IN_SECONDS =  24 * HOUR_IN_SECONDS;
+    private static final int WEEK_IN_SECONDS =  7 * DAY_IN_SECONDS;
+
     public static String getRelativeTimeAgo(String rawJsonDate) {
         String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
         SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
@@ -19,8 +24,19 @@ public class ConvertJsonDate {
         String relativeDate = "";
         try {
             long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            long currTimeMillis = System.currentTimeMillis();
+            long timeDiff = (currTimeMillis - dateMillis) / 1000;
+            if (timeDiff / WEEK_IN_SECONDS > 0) {
+                return String.format("%dw", timeDiff / WEEK_IN_SECONDS);
+            } else if (timeDiff / DAY_IN_SECONDS > 0 ) {
+                return String.format("%dd", timeDiff / DAY_IN_SECONDS);
+            } else if (timeDiff / HOUR_IN_SECONDS > 0 ){
+                return String.format("%dh", timeDiff / HOUR_IN_SECONDS);
+            } else if (timeDiff / MINUTE_IN_SECONDS > 0){
+                return String.format("%dm", timeDiff / MINUTE_IN_SECONDS);
+            } else {
+                return String.format("%ds", timeDiff);
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
